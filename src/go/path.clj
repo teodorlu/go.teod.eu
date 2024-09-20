7de443f7c3f766f7afdc714075ac544b6e83f253
Teodor Heggelund <git@teod.eu>
2024-09-20 21:40:53 +0200

    Remove unused

--- src/go/path.clj
+++ src/go/path.clj
@@ -1,18 +1,4 @@
 (ns go.path)
 
 (def index "/")
-(def other "/other")
-(def other2 "/other2")
-(def other3 "/other3")
-(def other4 "/other4")
-(def ^{:deprecated "2024-07-29"
-       :deprecation/reason "indented paragraph looks weird / chaotic."}
-  other5
-  "/other5")
-(def other6 "/other6")
-(def theme-code "/theme-code")
-(def theme-line "/theme-line")
-(def theme-nu "/theme-nu")
-
-(def icon-web "/icon-web.svg")
 (def play-teod-eu "https://play.teod.eu")
