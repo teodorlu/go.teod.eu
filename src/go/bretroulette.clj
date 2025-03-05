868e03416f5b03e3349f93c0ff4c814050bda97c
Teodor Heggelund <git@teod.eu>
2025-03-05 10:49:07 +0100

    Avoid dead ends in Bretroulette
    Bret has added a navbar! We don't want to roll the navbar when we're looking for
    mind-bending ideas.

--- src/go/bretroulette.clj
+++ src/go/bretroulette.clj
@@ -5,21 +5,29 @@
 
 ;; https://worrydream.com/refs/ is a treasure trove!
 
-(def refs-url "https://worrydream.com/refs/")
+(defn find-reference-elements [hiccup]
+  (->> hiccup
+       (lookup/select 'a)
+       ;; .lunchbox links are navigation, not references
+       (remove (set (lookup/select '[.lunchbox a] hiccup)))))
 
 (defn refs-url->references [url]
-  (->> (slurp url)
-       hickory/parse hickory/as-hiccup
-       (filter vector?)
-       first
-       (lookup/select 'a)
+  (->> url slurp hickory/parse hickory/as-hiccup
+       find-reference-elements
        (map (fn [ref-hiccup]
               {:text (lookup/text ref-hiccup)
                :uri (str url (:href (lookup/attrs ref-hiccup)))}))))
+
+(def refs-url "https://worrydream.com/refs/")
 
 (def references (future (refs-url->references refs-url)))
 
 (defn random-ref []
   (rand-nth @references))
 
-#_(random-ref)
+(comment
+  (random-ref)
+  (def hiccup (hickory/as-hiccup (hickory/parse (slurp refs-url))))
+  (find-reference-elements hiccup)
+
+  )
