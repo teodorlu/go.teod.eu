1cb65576971b50242a1cf7e66b5abda9fcf5b9d4
Teodor Heggelund <git@teod.eu>
2025-03-09 16:07:29 +0100

    Jeg vil ha mer design.

---
+++ src/go/notes.clj
@@ -0,0 +1,17 @@
+(ns go.notes)
+
+;; I want to switch between different designs
+;;
+;; Just read [Public draft] The niche design zine
+;; original: https://docs.google.com/document/d/1Qjoy-JYwS6GKXaa7O828lT5kwfycDr9RHKrDCG2dXuk/edit?tab=t.0
+;; copy, PDF: https://nextcloud.teod.eu/f/163606
+;;
+;; Why not throw it around? Try something different?
+
+;; Portfolio on Mikrobloggeriet?
+
+;; Differnt designs on go.teod.eu?
+
+;; and not just different styles, different _designs_.
+;; where portfolio shows completely different variations.
+;; that's where I want to go.
