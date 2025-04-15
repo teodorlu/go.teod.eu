0d5971c27f41b64e9a15a01980b562987bf3d068
Teodor Heggelund <git@teod.eu>
2025-04-15 11:30:13 +0200

    Introduce namespaces for reference data model

--- src/go/refs2.clj
+++ src/go/refs2.clj
@@ -18,26 +18,39 @@
   {:title "Design and quality"
    :subtitle "On how to make things, what makes things good, and how to make good things."
    :refs
-   #{{:title "Creative Computation"
-      :author "Jack Rusher"
-      :year "2019"
-      :source "https://www.youtube.com/watch?v=TeXCvh5X5w0"
-      :novelty "An argument that design and engineering are and should be closely tied together"}
-     {:title "Designerly ways of knowing"
-      :author "Nigel Cross"
-      :year "1982"
-      :source "https://oro.open.ac.uk/39253/8/Designerly%20Ways%20of%20Knowing%20DS.pdf"
+   #{{:text/title "Creative Computation"
+      :text/author "Jack Rusher"
+      :text/published "2019"
+      :text/source "https://www.youtube.com/watch?v=TeXCvh5X5w0"
+      :novelty "An argument that design and engineering are and should be closely connected"}
+     {:text/title "Designerly ways of knowing"
+      :text/author "Nigel Cross"
+      :text/published "1982"
+      :text/source "https://oro.open.ac.uk/39253/8/Designerly%20Ways%20of%20Knowing%20DS.pdf"
       :novelty "Explains what design IS"}
-     {:title "Taste"
-      :author "Peter Seibel"
-      :year "2015"
-      :source "https://gigamonkeys.com/taste/"}
-     {:title "The niche design zine [Public draft]"
-      :author "Itay Dreyfus"
-      :year "2025"
-      :source "https://docs.google.com/document/d/1Qjoy-JYwS6GKXaa7O828lT5kwfycDr9RHKrDCG2dXuk/edit?tab=t.0"
+     {:text/title "Taste"
+      :text/author "Peter Seibel"
+      :text/published "2015"
+      :text/source "https://gigamonkeys.com/taste/"}
+     {:text/title "The niche design zine [Public draft]"
+      :text/author "Itay Dreyfus"
+      :text/published "2025"
+      :text/source "https://docs.google.com/document/d/1Qjoy-JYwS6GKXaa7O828lT5kwfycDr9RHKrDCG2dXuk/edit?tab=t.0"
       :novelty "Makes a case for caring about and experimenting with design"}
      }})
+
+(def refs-clojure-europe-easter-2025
+  {:title "Clojure Europe Easter 2025 reading recommendations"
+   :refs
+   #{{:text/title "The Illiad"
+      :text/author "Homer"
+      :text/published "8th century BC"
+      :text/comments
+      #{{:ref/by "Matthias"
+         :ref/commentary "If you can find good modern translations, and can deal with a story written in verse, they are great."}}}
+     {:text/title "The Odyssey"
+      :text/author "Homer"
+      :text/published "8th century BC"}}})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; LOGIC
@@ -48,9 +61,9 @@
           [::rounded/text
            [::rounded/core title]
            subtitle]]
-         (for [{:keys [title author year source novelty]} (sort-by :title refs)]
-           [::rounded/text [::rounded/link source title]
-            (str "(" year ", " author ")")
+         (for [{:as text :keys [novelty]} (sort-by :text/title refs)]
+           [::rounded/text [::rounded/link (:text/source text) (:text/title text)]
+            (str "(" (:text/published text) ", " (:text/author text) ")")
             novelty]))])
 
 (defn navitation->content [navigation]
@@ -82,3 +95,30 @@
       [(title+lines->content why)
        (refs->content refs-design-and-quality)
        (navitation->content path/navigation)])])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FOOTNOTES
+
+(comment
+  ;; Challenge:
+  ;;
+  ;; - define a data format that works for different kinds of references
+  ;; - present the different kinds of references neatly.
+
+  ;; Ref example: paper, with novelty and source
+  {:title "Designerly ways of knowing"
+   :author "Nigel Cross"
+   :text/published "1982"
+   :source "https://oro.open.ac.uk/39253/8/Designerly%20Ways%20of%20Knowing%20DS.pdf"
+   :novelty "Explains what design IS"}
+
+  ;; Ref example: history book, published year and recommended by
+  {:title "Guns of August"
+   :text/published 1962
+   :author "Barbara W. Tuchman"
+   :type "book"
+   :genre "history"
+   :recommended-by "Mario Trost and Jack Rusher"}
+
+  ;; (I find the data modeling part of this job to be challenging!)
+  )
