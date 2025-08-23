39d988344c13eadb28f860eb894a5837480c97b8
Teodor Heggelund <git@teod.eu>
2025-08-23 20:04:48 +0200

    go beautiful too

---
+++ src/go/beautiful/too.clj
@@ -0,0 +1,26 @@
+(ns go.beautiful.too
+  {:url "https://beautiful.teod.eu"})
+
+["Signs of beauty"
+ "Got a million ideas?"
+ ["Great." "That means you're a curious person."
+  "You like to learn things."
+  "Your next problem is going to be synthesis."
+  ;; "Your next problem is going to be synthesis." presumes!
+  ;; Not necessarily correct.
+  "What binds your ideas together?"]
+ ["Start looking for topic specific signs of beauty."
+  "When you're deep into a topic, skill or specialization, ask yourself what beauty is."]
+ {:beautiful/code [;; beautiful.teod.eu/code
+                   "information is at hand"
+                   "as simple as possible"
+                   "support additative change"]
+  :beautiful/organizations [;; beautiful.teod.eu/organizations
+                            "intent"
+                            "trust"
+                            ]
+  :beautiful/theory ["explains well"
+                     "hard to change"]
+  :beautiful/practice ["direct connection to artefact"
+                       ""]}
+ ]
